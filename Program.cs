203e59a0c02ac433bbb97b3a7583204afde6e5b2
Bilal-Makic <b.makic@HTBLALeonding.onmicrosoft.com>
2023-03-21 12:31:39 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Collections.Concurrent;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -9,69 +11,85 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
+var colors = new ConcurrentDictionary<string, Color>(){
+    ["black"] = new(0, 1, 0),
+    ["brown"] = new(1, 10, 1),
+    ["red"] = new(2, 100, 2),
+    ["orange"] = new(3, 1000, 0),
+    ["yellow"] = new(4, 10000, 0),
+    ["green"] = new(5, 100000, 0.5),
+    ["blue"] = new(6, 1000000, 0.25),
+    ["violet"] = new(7, 10000000, 0.1),
+    ["grey"] = new(8, 100000000, 0.05),
+    ["white"] = new(9, 1000000000, 0),
+    ["gold"] = new(0, 0.1, 5),
+    ["silver"] = new(0, 0.01, 10)
+};
+
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/colors", () => new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white", "gold", "silver" });
+app.MapGet("/colors", () => colors.Keys)
+    .WithTags("Colors")
+    .WithOpenApi(o =>
+    {
+        o.Summary = "Returns all colors for bands on resistors";
+
+        o.Responses[((int)StatusCodes.Status200OK).ToString()].Description = "A list of all colors";
+
+        return o;
+    });
+
+
 app.MapGet("/colors/{color}", (string color) => 
 {
-    if(true){
-        return Results.Ok(new Color(0, 0, 0));
+    if(colors.TryGetValue(color, out var value))
+    {
+        return Results.Ok(value);
     }
     else
     {
         return Results.NotFound();
     }
 })
+    .WithTags("Colors")
     .Produces<Color>(StatusCodes.Status200OK)
     .Produces(StatusCodes.Status404NotFound)
     .WithOpenApi(o =>
     {
-        o.Description = "Get a color";
-        o.Responses[((int)StatusCodes.Status200OK).ToString()].Description = "Details for the color band";
+        o.Summary = "Returns a color for bands on resistors";
+        o.Parameters[0].Description = "The color to get";
+        o.Responses[((int)StatusCodes.Status200OK).ToString()].Description = "A color";
         o.Responses[((int)StatusCodes.Status404NotFound).ToString()].Description = "Color not found";
         return o;
     });
 
 app.MapGet("resistors/value-from-bands", (string firstBand, string secondBand, string thirdBand, string multiplier, string tolerance) => 
 {
-    if(true){
-        return Results.Ok(new ResistorValues(0, 0));
-    }
-    else
-    {
-        return Results.NotFound();
-    }
-})
-    .Produces<ResistorValues>(StatusCodes.Status200OK)
-    .Produces(StatusCodes.Status404NotFound)
-    .WithOpenApi(o =>
-    {
-        o.Description = "Get a resistor value from bands";
-        o.Responses[((int)StatusCodes.Status200OK).ToString()].Description = "Resistor value could be decoded correctly";
-        o.Responses[((int)StatusCodes.Status404NotFound).ToString()].Description = "Resistor value not found";
-        return o;
-    });
+    
+});
+
 
 app.Run();
 
-Dictionary<string, Color> colors = new()
-{
-    { "black", new Color(0, 1, 0) },
-    { "brown", new Color(1, 10, 1) },
-    { "red", new Color(2, 100, 2) },
-    { "orange", new Color(3, 1000, 0) },
-    { "yellow", new Color(4, 10000, 0) },
-    { "green", new Color(5, 100000, 0.5) },
-    { "blue", new Color(6, 1000000, 0.25) },
-    { "violet", new Color(7, 10000000, 0.1) },
-    { "grey", new Color(8, 100000000, 0.05) },
-    { "white", new Color(9, 1000000000, 0) },
-    { "gold", new Color(0, 0.1, 5) },
-    { "silver", new Color(0, 0.01, 10) }
-};
 
 public record Color(int value, double multiplier, double tolerance);
 
 
-public record ResistorValues(int resistorValue, int tolerance);
+public record ResistorValues(int resistorValue, int tolerance){
+    public int ResistorValue { get; init; } = resistorValue;
 
-public record ResistorBands(string firstBand, string secondBand, string thirdBand, string multiplier, string tolerance);
+    public int Tolerance { get; init; } = tolerance;
+};
+
+public record ResistorBands(string firstBand, string secondBand, string? thirdBand, string multiplier, string tolerance){
+    
+    public string FirstBand { get; init; } = firstBand;
+
+    public string SecondBand { get; init; } = secondBand;
+
+    public string? ThirdBand { get; init; } = thirdBand;
+
+    public string Multiplier { get; init; } = multiplier;
+
+    public string Tolerance { get; init; } = tolerance;
+
+};
